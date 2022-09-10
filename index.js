ff2da5693309fd61b5136e5147eca55294db9925
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-09-10 18:58:06 +0100

    modification

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const generateHTMLpage = require('./src/pagetemplate');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const { isNumberObject } = require('util/types');
 
 // Array to push answers 
 const StaffData = [];
