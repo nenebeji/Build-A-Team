65c0c1bab06f632c00a8c59e3213b7a6dad557c6
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-09-10 17:34:05 +0100

    added employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,51 @@
 const Employee = require('../lib/Employee');
+
+describe ('Employee', () => {
+    describe('Initialization', () => {
+        it('should return an object when called with the "new" "keyword"', () => {
+            const employee = new Employee();
+        
+            expect(typeof(employee)).toBe('object');
+        });
+
+        it('should set "name", "id" and "email" when created', () => {
+            const employee = new Employee('Cindy', 224, 'cindy@test.com');
+        
+            expect(employee.name).toBe('Cindy');
+            expect(employee.id).toBe(224);
+            expect(employee.email).toBe('cindy@test.com');
+        });
+    });
+
+    describe('getName', () => {
+        it('should return the "name" value when called', () => {
+            const employee = new Employee('Cindy', 224, 'cindy@test.com');
+
+            expect(employee.getName()).toEqual('Cindy');
+        });
+    });
+
+    describe('getID', () => {
+        it('should return the "id" value when called', () => {
+            const employee = new Employee('Cindy', 224, 'cindy@test.com');
+
+            expect(employee.getID()).toEqual(224);
+        });
+    });
+
+    describe('getEmail', () => {
+        it('should return the "email" value when called', () => {
+            const employee = new Employee('Cindy', 224, 'cindy@test.com');
+
+            expect(employee.getName()).toEqual('cindy@test.com');
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return "Employee"', () => {
+            const employee = new Employee('Cindy', 224, 'cindy@test.com');
+
+            expect(employee.getRole()).toEqual('Employee');
+        });
+    });
+});
